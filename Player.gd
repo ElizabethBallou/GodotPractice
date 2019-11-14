e97d9bbf271e10f1cf2b3b56f0a06397244380be
ElizabethBallou <epb267@nyu.edu>
2019-11-14 14:52:26 -0500

    basic working platformer controls using GDScript

--- Player.gd
+++ Player.gd
@@ -1,18 +1,32 @@
 extends KinematicBody2D
 
+# this was created using a basic Godot tutorial: https://www.youtube.com/watch?v=wETY5_9kFtA
+
 var motion = Vector2()
 
+const UP = Vector2(0, -1)
 
+const SPEED = 200
+
+const GRAVITY = 20
+
+const JUMP_HEIGHT = -500
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += GRAVITY
 	
-	if Input.is_mouse_button_pressed(BUTTON_RIGHT):
-		motion.x = 100
-	elif Input.is_mouse_button_pressed(BUTTON_LEFT):
-		motion.x = -100
+	if Input.is_key_pressed(KEY_D):
+		motion.x = SPEED
+	elif Input.is_key_pressed(KEY_A):
+		motion.x = -SPEED
 	else:
 		motion.x = 0	
-	move_and_slide(motion)
+		
+	if is_on_floor():
+		# print("on floor")
+		if Input.is_key_pressed(KEY_W):
+			motion.y = JUMP_HEIGHT
+				
+	motion = move_and_slide(motion, UP)
 	pass
